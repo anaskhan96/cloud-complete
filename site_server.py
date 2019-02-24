15e182d4d8da7abdd0966fe87000deec65fcd14d
Anas Khan <anas.khan96@outlook.com>
2019-02-24 06:03:48 +0530

    adding authentication at endpoints

--- site_server.py
+++ site_server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, render_template_string, send_from_directory
+from flask_basicauth import BasicAuth
 from pymongo import MongoClient
 from datetime import datetime
 import os
@@ -8,12 +9,16 @@ db = client["ccbd-reports"]
 reports_collection = db["reports"]
 
 app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'ccbd-evaluator'
+app.config['BASIC_AUTH_PASSWORD'] = 'ccbdevalsecret'
+basic_auth = BasicAuth(app)
 
 @app.route('/ccbd')
 def hello():
     return "Testing"
 
 @app.route('/ccbd/reports', methods=['GET'])
+@basic_auth.required
 def all_reports():
     report_documents_cursor = reports_collection.find({})
     items = []
@@ -26,6 +31,7 @@ def all_reports():
     return render_template('reports.html', items=items)
 
 @app.route('/ccbd/reports/<team_id>/<date>')
+@basic_auth.required
 def report(team_id, date):
     report_document = reports_collection.find_one({
         'team_id': team_id,
