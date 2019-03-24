e31a0b96c2a1eb058c613d52816dd45a1eaddf00
Anas Khan <anas.khan96@outlook.com>
2019-03-24 21:00:47 +0530

    backend code to generate and view lb reports

--- site_server.py
+++ site_server.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 from datetime import datetime
 from test import generate
 from container_test import container_generate
+from lb_test import lb_generate
 from container_test import student_generate
 from werkzeug.utils import secure_filename
 import os
@@ -17,6 +18,7 @@ client = MongoClient('mongodb://anask.xyz:27017')
 db = client["ccbd-reports"]
 reports_collection = db["reports"]
 container_reports_collection = db["container_reports"]
+lb_reports_collection = db["lb_reports"]
 sample_acts_collection = db["sample_acts"]
 
 app = Flask(__name__)
@@ -60,6 +62,24 @@ def container_teamsupload():
     
     return jsonify(response)
 
+@app.route('/ccbd/lbGenerate', methods=['POST'])
+@basic_auth.required
+def lb_teamsupload():
+    team_file = request.files['lbTeams']
+    private_key_files = request.files.getlist('privateKeys[]')
+    for private_key_file in private_key_files:
+        private_key_file.save(secure_filename(private_key_file.filename))
+    response = {}
+    try:
+        res = lb_generate(team_file.read())
+        response['success'] = True
+        response['data'] = res
+    except:
+        response['success'] = False
+    
+    for private_key_file in private_key_files:
+        os.remove(secure_filename(private_key_file.filename))
+
 @app.route('/ccbd/reports', methods=['GET'])
 @basic_auth.required
 def all_reports():
@@ -85,6 +105,19 @@ def all_container_reports():
             'date': datetime.fromtimestamp(report['date']).strftime('%Y-%m-%d %H:%M:%S')
         })
     return render_template('reports.html', items=items, test="container")
+
+@app.route('/ccbd/lbReports', methods=['GET'])
+@basic_auth.required
+def all_lb_reports():
+    report_documents_cursor = lb_reports_collection.find({})
+    items = []
+    for report in report_documents_cursor:
+        items.append({
+            'team': report['team_id'],
+            'link': request.path + '/' + report['team_id'] + '/' + str(report['date']),
+            'date': datetime.fromtimestamp(report['date']).strftime('%Y-%m-%d %H:%M:%S')
+        })
+    return render_template('reports.html', items=items, test="lb")
 
 @app.route('/ccbd/reports/<team_id>', methods=['GET'])
 @app.route('/ccbd/reports/<team_id>/<date>', methods=['GET'])
@@ -128,6 +161,30 @@ def container_report(team_id, date=None):
         return render_template('reports.html', items=items, test="container")
     
     report_document = container_reports_collection.find_one({
+        'team_id': team_id,
+        'date': float(date)
+    })
+    encoded_report = report_document['encoded_report']
+    return render_template_string(encoded_report)
+
+@app.route('/ccbd/lbReports/<team_id>', methods=['GET'])
+@app.route('/ccbd/lbReports/<team_id>/<date>', methods=['GET'])
+@basic_auth.required
+def lb_report(team_id, date=None):
+    if not date:
+        report_documents_cursor = lb_reports_collection.find({
+            'team_id': team_id
+        })
+        items = []
+        for report in report_documents_cursor:
+            items.append({
+                'team': report['team_id'],
+                'link': request.path + '/' + report['team_id'] + '/' + str(report['date']),
+                'date': datetime.fromtimestamp(report['date']).strftime('%Y-%m-%d %H:%M:%S')
+            })
+        return render_template('reports.html', items=items, test="lb")
+    
+    report_document = lb_reports_collection.find_one({
         'team_id': team_id,
         'date': float(date)
     })
