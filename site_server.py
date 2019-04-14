058db82434be945f5b5c79ee7fe0174f619951fc
Anas Khan <anas.khan96@outlook.com>
2019-04-14 13:53:46 +0530

    adding orch_template for report

--- site_server.py
+++ site_server.py
@@ -6,8 +6,10 @@ from datetime import datetime
 from test import generate
 from container_test import container_generate
 from lb_test import lb_generate
+from orch_test import orch_generate
 from container_test import student_generate
 from lb_test import lb_student_generate
+from orch_test import orch_student_generate
 from werkzeug.utils import secure_filename
 import os
 import sys
@@ -20,7 +22,7 @@ db = client["ccbd-reports"]
 reports_collection = db["reports"]
 container_reports_collection = db["container_reports"]
 lb_reports_collection = db["lb_reports"]
-sample_acts_collection = db["sample_acts"]
+orch_reports_collection = db["orch_reports"]
 
 app = Flask(__name__)
 app.config['BASIC_AUTH_USERNAME'] = 'ccbd-evaluator'
@@ -39,7 +41,6 @@ def teamsupload():
     try:
         res = generate(file.read())
         return jsonify({'success':True, 'data': res})
-        #return Response(generate(file.read()), mimetype='application/json')
     except:
         return jsonify({'success':False})
 
@@ -89,6 +90,29 @@ def lb_teamsupload():
 
     return jsonify(response)
 
+@app.route('/ccbd/orchGenerate', methods=['POST'])
+@basic_auth.required
+def orch_teamsupload():
+    team_file = request.files['orchTeams']
+    private_key_files = request.files.getlist('privateKeys[]')
+    for private_key_file in private_key_files:
+        private_key_file.save(secure_filename(private_key_file.filename))
+    response = {}
+    try:
+        res = orch_generate(team_file.read())
+        response['success'] = True
+        response['data'] = res
+    except:
+        response['success'] = False
+    
+    for private_key_file in private_key_files:
+        try:
+            os.remove(secure_filename(private_key_file.filename))
+        except:
+            continue
+    
+    return jsonify(response)
+
 @app.route('/ccbd/reports', methods=['GET'])
 @basic_auth.required
 def all_reports():
@@ -127,6 +151,19 @@ def all_lb_reports():
             'date': datetime.fromtimestamp(report['date']).strftime('%Y-%m-%d %H:%M:%S')
         })
     return render_template('reports.html', items=items, test="lb")
+
+@app.route('/ccbd/orchReports', methods=['GET'])
+@basic_auth.required
+def all_orch_reports():
+    report_documents_cursor = orch_reports_collection.find({})
+    items = []
+    for report in report_documents_cursor:
+        items.append({
+            'team': report['team_id'],
+            'link': request.path + '/' + report['team_id'] + '/' + str(report['date']),
+            'date': datetime.fromtimestamp(report['date']).strftime('%Y-%m-%d %H:%M:%S')
+        })
+    return render_template('reports.html', items=items, test="orch")
 
 @app.route('/ccbd/reports/<team_id>', methods=['GET'])
 @app.route('/ccbd/reports/<team_id>/<date>', methods=['GET'])
@@ -200,6 +237,30 @@ def lb_report(team_id, date=None):
     encoded_report = report_document['encoded_report']
     return render_template_string(encoded_report)
 
+@app.route('/ccbd/orchReports/<team_id>', methods=['GET'])
+@app.route('/ccbd/orchReports/<team_id>/<date>', methods=['GET'])
+@basic_auth.required
+def orch_report(team_id, date=None):
+    if not date:
+        report_documents_cursor = orch_reports_collection.find({
+            'team_id': team_id
+        })
+        items = []
+        for report in report_documents_cursor:
+            items.append({
+                'team': report['team_id'],
+                'link': request.path + '/' + report['team_id'] + '/' + str(report['date']),
+                'date': datetime.fromtimestamp(report['date']).strftime('%Y-%m-%d %H:%M:%S')
+            })
+        return render_template('reports.html', items=items, test="orch")
+    
+    report_document = orch_reports_collection.find_one({
+        'team_id': team_id,
+        'date': float(date)
+    })
+    encoded_report = report_document['encoded_report']
+    return render_template_string(encoded_report)
+
 @app.route('/ccbd/studentViewing', methods=['GET'])
 def student_view():
     return render_template('studentView.html')
@@ -239,6 +300,24 @@ def student_generate_lb_report(lbDns, actsIp, actsUsername, usersIp, usersUserna
     try:
         os.remove(secure_filename(acts_private_key_file.filename))
         os.remove(secure_filename(users_private_key_file.filename))
+    except:
+        pass
+    return jsonify(response)
+
+@app.route('/ccbd/studentViewing/<selfielessIp>/<actsIp>/<actsUsername>', methods=['POST'])
+def student_generate_orch_report(selfielessIp, actsIp, actsUsername):
+    acts_private_key_file = request.files['actsPrivateKey']
+    acts_private_key_file.save(secure_filename(acts_private_key_file.filename))
+    response = {}
+    try:
+        res = orch_student_generate(selfielessIp, actsIp, actsUsername, secure_filename(acts_private_key_file.filename))
+        response['success'] = True
+        response['data'] = res
+    except:
+        response['success'] = False
+    
+    try:
+        os.remove(secure_filename(acts_private_key_file.filename))
     except:
         pass
     return jsonify(response)
